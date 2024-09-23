f5189f174aceecc0c6b039c7f661d97dbbcf7d7d
VishK98 <visheshkumar41@gmail.com>
2024-09-23 11:58:32 +0530

    side bar fixed

--- src/admin/components/Sidebar.jsx
+++ src/admin/components/Sidebar.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/actions/user";
 
 const Sidebar = ({ isClosed }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation(); 
 
   const logoutHandler = () => {
     dispatch(logout());
-    navigate("/login"); // Redirect user to login page after logout
+    navigate("/"); 
+  };
+
+  const isActive = (path) => {
+    return location.pathname === path ? "active" : "";
   };
 
   return (
@@ -21,42 +26,42 @@ const Sidebar = ({ isClosed }) => {
         </div>
       </Link>
       <ul className="side-menu">
-        <li>
+        <li className={isActive("/dashboard")}>
           <Link to="/dashboard">
             <i className="bx bxs-dashboard"></i>Dashboard
           </Link>
         </li>
-        <li className="active">
+        <li className={isActive("/products")}>
           <Link to="/products">
             <i className="bx bx-box"></i>Products
           </Link>
         </li>
-        <li>
+        <li className={isActive("/categories")}>
           <Link to="/categories">
             <i className="bx bx-category-alt"></i>Categories
           </Link>
         </li>
-        <li>
+        <li className={isActive("/purchases")}>
           <Link to="/purchases">
             <i className="bx bx-cart"></i>Purchases
           </Link>
         </li>
-        <li>
+        <li className={isActive("/sales")}>
           <Link to="/sales">
             <i className="bx bx-line-chart"></i>Sales
           </Link>
         </li>
-        <li>
+        <li className={isActive("/billings")}>
           <Link to="/billings">
             <i className="bx bx-receipt"></i>Billings
           </Link>
         </li>
-        <li>
+        <li className={isActive("/reports")}>
           <Link to="/reports">
             <i className="bx bx-bar-chart-alt-2"></i>Reports
           </Link>
         </li>
-        <li>
+        <li className={isActive("/settings")}>
           <Link to="/settings">
             <i className="bx bx-cog"></i>Settings
           </Link>
@@ -64,7 +69,7 @@ const Sidebar = ({ isClosed }) => {
       </ul>
       <ul className="side-menu">
         <li>
-          <Link to="#" className="logout" onClick={logoutHandler}>
+          <Link className="logout" onClick={logoutHandler}>
             <i className="bx bx-log-out-circle"></i>
             Logout
           </Link>
